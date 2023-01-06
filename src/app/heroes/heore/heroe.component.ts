6b5aa58cbde02c12892b5621045ae38da2bb40f4
GustavoSepul <gsepulvedao@ing.ucsc.cl>
2023-01-06 11:58:56 -0300

    cap 45 curso angular
    one way data binding - enlazado en una sola vía

--- src/app/heroes/heore/heroe.component.ts
+++ src/app/heroes/heore/heroe.component.ts
@@ -12,13 +12,21 @@ export class HeroreComponent{
   nombre: string = 'Ironman';
   edad: number = 45
 
-  get nombreCapitalizado() {
+  get nombreCapitalizado(): string {
       return this.nombre.toUpperCase();
   }
 
   obtenerNombre(): string{
     // return this.nombre + ' - ' + this.edad.toString()
     return `${ this.nombre } - ${ this.edad }`; //es lo mismo que arriba
+  }
+
+  cambiarNombre():void {
+    this.nombre = 'Spiderman';
+  }
+
+  cambiarEdad():void {
+    this.edad = 25;
   }
 
 }
