0838bcc2b511e9142ff03f98553d3e33661d4187
GustavoSepul <gsepulvedao@ing.ucsc.cl>
2023-01-06 16:28:44 -0300

    cap 64 curso angular
    Trabajo con inputs y módulos

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -18,24 +18,11 @@ export class MainPageComponent {
     }
   ];
 
+
   nuevo: Personaje = {
-    nombre: '',
-    poder: 0
+    nombre: 'Maestro Roshi',
+    poder: 1000
   }
 
-  // cambiarNombre( event:any ) {
-  //   console.log( event.target.value )
-  // }
-
-  agregar() {
-    if( this.nuevo.nombre.trim().length == 0 ) { return; }
-    console.log( this.nuevo )
-
-    this.personajes.push( this.nuevo );
-    this.nuevo = {
-      nombre: '',
-      poder: 0
-    }
-  }
 
 }
