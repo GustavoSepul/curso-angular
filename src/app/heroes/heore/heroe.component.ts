f53d270c6eb937371288ed2e9b6bdc728db55712
GustavoSepul <gsepulvedao@ing.ucsc.cl>
2023-01-06 11:51:12 -0300

    cap 44 curso angular
    cambios template de componente y creacion de tabla con estos

--- src/app/heroes/heore/heroe.component.ts
+++ src/app/heroes/heore/heroe.component.ts
@@ -9,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class HeroreComponent{
 
+  nombre: string = 'Ironman';
+  edad: number = 45
+
+  get nombreCapitalizado() {
+      return this.nombre.toUpperCase();
+  }
+
+  obtenerNombre(): string{
+    // return this.nombre + ' - ' + this.edad.toString()
+    return `${ this.nombre } - ${ this.edad }`; //es lo mismo que arriba
+  }
+
 }
