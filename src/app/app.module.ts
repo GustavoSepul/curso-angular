907a1ee3eecaa0d7c44ef9e84965ae6123192ee3
GustavoSepul <gsepulvedao@ing.ucsc.cl>
2023-01-06 12:15:15 -0300

    cap 46 curso angular
    Crear componente de forma automática

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { HeroreComponent } from './heroes/heore/heroe.component';
+import { ListadoComponent } from './heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContadorComponent,
-    HeroreComponent
+    HeroreComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule
